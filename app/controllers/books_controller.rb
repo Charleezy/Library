8c3dbcc2f2875ffd5d3043c4beea27d9d6b98f7b
Charlie G <obesechicken13@yahoo.com>
2015-03-30 02:19:43 -0400

    tried setting up books controller to show button, didn't work

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,19 +7,35 @@ class BooksController < ApplicationController
   end 
     
   def edit
-      if session[:admin]? @book = Book.find(params[:id]
+      if session[:admin]
+          @book = Book.find(params[:id])
+      end
   end
 
   def update
-      @book = book.find(params[:id])
-      if @book.update_attributes(book_params)
-        redirect_to('index')
+      if session[:admin]
+          @book = Book.find(params[:id])
+          if @book.update_attributes(book_params)
+            redirect_to('index')
+          else
+            render('index')
+          end
       else
-        render('index')
-      end
+          redirect_to @index and return
+      end   
   end          
     
   def book_params
       params.require(:book).permit(:name, :isbn, :status)
+  end
+          
+  private
+  
+  def sort_column
+    Book.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
