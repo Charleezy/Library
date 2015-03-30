8a51855fe9d7bd23464c2d06d90b843739476f87
Charlie G <obesechicken13@yahoo.com>
2015-03-30 01:27:20 -0400

    slight modification, added update method to controller and gave edit controller some logic

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,17 @@ class BooksController < ApplicationController
   end 
     
   def edit
+      if session[:admin]? @book = Book.find(params[:id]
   end
+
+  def update
+      @book = book.find(params[:id])
+      if @book.update_attributes(book_params)
+        redirect_to('index')
+      else
+        render('index')
+      end
+  end          
     
   def book_params
       params.require(:book).permit(:name, :isbn, :status)
